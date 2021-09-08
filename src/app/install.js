55888980bb086974f876551433b67cce7d774f9e
Yeinso Blanco <yeinsojbm@gmail.com>
2021-09-07 19:59:29 -0500

    Install some

--- src/app/install.js
+++ src/app/install.js
@@ -1,13 +1,29 @@
 import { useTranslate } from 'react-translate';
 
+import { Grid, GridItem, Center, Circle } from "@chakra-ui/react"
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+
 
 const Install = ({ db, setError, setLoading, settings }) => {
   const t = useTranslate('install');
 
   return (
-    <>
-      {t('welcome')}
-    </>
+    <Grid
+      h="200px"
+      gap={2}
+    >
+      <GridItem colSpan={4} center >
+        <Center>
+          {t('welcome')}
+        </Center>
+      </GridItem>
+      <GridItem colSpan={2} bg="papayawhip" />
+      <GridItem colSpan={2}>
+        <Circle size="10em" bg="green" color="white">
+          <ArrowForwardIcon />
+        </Circle>
+      </GridItem>
+    </Grid>
   )
   
 }
